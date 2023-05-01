c5e5760b59e0ac931ac45c9a32ed8de16eb87917
gio797 <119562363+gio797@users.noreply.github.com>
2023-05-02 03:42:05 +0400

    adding NavLinks

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
 import HostLayout from "./components/HostLayout"
 
 
@@ -18,14 +20,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="vans" element={<Vans />}/>
+          <Route path="vans/:id" element={<VanDetail />}/>
 
-          <Route path="/host" element={<HostLayout />}>
+          <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />}/>
-            <Route path="/host/reviews" element={<Reviews />}/>
+            <Route path="income" element={<Income />}/>
+            <Route path="reviews" element={<Reviews />}/>
+            <Route path="vans" element={<HostVans />}/>
+            <Route path="vans/:id" element={<HostVanDetail />}/>
           </Route>
         </Route>
       </Routes>
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,5 @@
+export default function HostVans() {
+    return(
+        <h1>hostvans here</h1>
+    )
+}
