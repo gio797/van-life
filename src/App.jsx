7a72e97749463c0b3f0b35dc738c47a61f83c29f
gio797 <119562363+gio797@users.noreply.github.com>
2023-05-01 23:41:23 +0400

    adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,14 @@ function App() {
     
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
           <Route path="/about" element={<About />}/>
           <Route path="/vans" element={<Vans />}/>
           <Route path="/vans/:id" element={<VanDetail />}/>
+
           <Route path="/host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
             <Route path="/host/income" element={<Income />}/>
             <Route path="/host/reviews" element={<Reviews />}/>
           </Route>
