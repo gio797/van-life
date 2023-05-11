55c2490b8bf7df069bc914cf52a56babcff7365d
gio797 <119562363+gio797@users.noreply.github.com>
2023-05-11 12:47:58 +0400

    autent happy, sad path, usenavigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useNavigate } from "react-router-dom"
 import React, {useState} from "react"
+import { loginUser } from "../api"
 
 export function loader({request}) {
     return new URL(request.url).searchParams.get("message")
@@ -7,11 +8,21 @@ export function loader({request}) {
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+    const [status, setStatus] = useState("idle")
+    const [error, setError] = useState(null)
     const message = useLoaderData()
+    const navigate = useNavigate()
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        setError(null)
+        loginUser(loginFormData)
+            .then(data => {
+                navigate("/host", {replace: true})
+            })
+            .catch(err => setError(err))
+            .finally(() => setStatus("idle"))
     }
 
     function handleChange(e) {
@@ -26,6 +37,7 @@ export default function Login() {
         <div className="login-container">
             <h1>Sign in to your account</h1>
             {message && <h3 className="red">{message}</h3>}
+            {error && <h3 className="red">{error.message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -41,8 +53,11 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button 
+                disabled={status === 'submitting'}>
+                    {status === 'submitting' ? "Logging in...": "Log in"}
+                </button>
             </form>
         </div>
-    )
+    ) 
 }
